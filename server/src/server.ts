f17d0c1c355f6930b44ab2fe715ff15d55bb5388
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-09-17 16:41:38 -0300

    feat: Server running

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,11 @@
 import express from "express";
 
 const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Server running" });
+});
+
+app.listen(3333, () =>
+  console.log("🚀🚀 Server running in port:", 3333, "🚀🚀")
+);
