5b005e9e572afa309759776980595b3efd15a95a
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-09-18 18:25:46 -0300

    feat: routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,10 @@
-import express from "express";
+import express from 'express'
+import { appRoutes } from './routes'
 
-const app = express();
-
-app.get("/", (request, response) => {
-  return response.json({ message: "Server running" });
-});
+const app = express()
+app.use(express.json())
+app.use(appRoutes)
 
 app.listen(3333, () =>
-  console.log("🚀🚀 Server running in port:", 3333, "🚀🚀")
-);
+	console.log('🚀🚀 Server running in port:', 3333, '🚀🚀'),
+)
