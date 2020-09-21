52ade08205d1ac8210ce842e7c69b32656c64a90
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-09-21 20:59:31 -0300

    feat: express.static uploads

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
+import { resolve } from 'node:path'
 import { appRoutes } from './routes'
 
 const app = express()
 app.use(express.json())
 app.use(appRoutes)
+
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333, () =>
 	console.log('🚀🚀 Server running in port:', 3333, '🚀🚀'),
