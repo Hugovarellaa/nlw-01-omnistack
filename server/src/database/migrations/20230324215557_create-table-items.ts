593cef4b7d757710bd2dbbda701cd160e5699e7e
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-09-22 21:00:23 -0300

    feat: Upload table in database

--- server/src/database/migrations/20230324215557_create-table-items.ts
+++ server/src/database/migrations/20230324215557_create-table-items.ts
@@ -2,7 +2,7 @@ import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
 	await knex.schema.createTable('items', (table) => {
-		table.uuid('id').primary()
+		table.increments('id').primary().notNullable()
 		table.string('image').notNullable()
 		table.string('title').notNullable()
 	})
