dc1b06fb8da6b387a4798f68541dd642ef045445
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-09-22 21:31:02 -0300

    feat: alter id-uuid for id-increments

--- server/src/database/migrations/20230324223555_create-table-points-items.ts
+++ server/src/database/migrations/20230324223555_create-table-points-items.ts
@@ -2,9 +2,9 @@ import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
 	await knex.schema.createTable('point_items', (table) => {
-		table.uuid('id').primary()
-		table.uuid('point_id').references('id').inTable('points')
-		table.uuid('item_id').references('id').inTable('items')
+		table.increments('id').primary().notNullable()
+		table.integer('point_id').references('id').inTable('points')
+		table.integer('item_id').references('id').inTable('items')
 	})
 }
 
