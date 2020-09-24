6cd25564c68ba2445d83551985ffda1f25fc6e33
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-09-23 21:54:12 -0300

    feat: completed PointsController

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -1,1 +1,40 @@
-export class PointsController {}
+import { Request, Response } from 'express'
+import { knex } from '../database/connection'
+
+export class PointsController {
+	async handler(request: Request, response: Response) {
+		const { name, email, whatsapp, latitude, longitude, city, uf, items } =
+			request.body
+
+		const trx = await knex.transaction()
+
+		const point = {
+			image: 'faker',
+			name,
+			email,
+			whatsapp,
+			latitude,
+			longitude,
+			city,
+			uf,
+		}
+
+		const insertedIds = await trx('points').insert(point)
+
+		const point_id = insertedIds[0]
+
+		const pointItem = items.map((item_id: number) => {
+			return {
+				item_id,
+				point_id,
+			}
+		})
+
+		await trx('point_items').insert(pointItem)
+
+		return response.status(201).json({
+			id: point_id,
+			...point,
+		})
+	}
+}
