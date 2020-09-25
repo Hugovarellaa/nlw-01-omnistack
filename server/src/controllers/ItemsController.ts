87900b7137085bd0df0c8b27eb30366ef9e4c45a
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-09-24 22:39:52 -0300

    refactor: using ItensController

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -1,1 +1,18 @@
-export class ItemsController {}
+import { Request, Response } from 'express'
+import { knex } from '../database/connection'
+
+export class ItemsController {
+	async index(request: Request, response: Response) {
+		const items = await knex('items').select('*')
+
+		const serializedItems = items.map((items) => {
+			return {
+				id: items.id,
+				title: items.title,
+				image_url: `http://localhost:3333/uploads/${items.image}`,
+			}
+		})
+
+		return response.json(serializedItems)
+	}
+}
