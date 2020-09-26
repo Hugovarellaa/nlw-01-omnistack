e92914e202adc75550b3d41cf1b137bbdb02d1f8
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-09-25 22:49:22 -0300

    feat: Get point for items specification

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -12,7 +12,15 @@ export class PointsController {
 			return response.status(400).json({ message: 'Point not found!' })
 		}
 
-		return response.json(point)
+		const items = await knex('items')
+			.join('point_items', 'items.id', '=', 'point_items.item_id')
+			.where('point_items.point_id', id)
+			.select('items.title')
+
+		return response.json({
+			point,
+			items,
+		})
 	}
 
 	async create(request: Request, response: Response) {
