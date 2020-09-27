97ca9fb517f4020b52cac1a0d23e38716084428d
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-09-27 11:11:45 -0300

    feat: path file database

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,20 +1,21 @@
 import { Knex, knex as setupKnex } from 'knex'
+import { resolve } from 'node:path'
 
 export const knexConfig: Knex.Config = {
 	client: 'sqlite',
 	connection: {
-		filename: './src/database/app.db',
+		filename: resolve(__dirname, 'app.db'),
 	},
 	useNullAsDefault: true,
 
 	migrations: {
 		extension: 'ts',
-		directory: './src/database/migrations',
+		directory: resolve(__dirname, 'migrations'),
 	},
 
 	seeds: {
 		extension: 'ts',
-		directory: './src/database/seeds',
+		directory: resolve(__dirname, 'seeds'),
 	},
 }
 
