c9efa0951d88f909bb83a9045b278045796d6f5d
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-09-28 11:30:30 -0300

    feat: create table points

--- server/src/database/migrations/20230325141429_create-table-points.ts
+++ server/src/database/migrations/20230325141429_create-table-points.ts
@@ -1,10 +1,13 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable('points', (table) => {
+		table.increments('id').primary().notNullable()
+		table.string('image').notNullable()
+		table.string('title').notNullable()
+	})
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+	await knex.schema.dropTable('points')
 }
-
