3f00f239793c0b9176f112b83b56295b396021f4
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-09-28 11:23:28 -0300

    feat: create table-items

--- server/src/database/migrations/20230325141437_create-table-items.ts
+++ server/src/database/migrations/20230325141437_create-table-items.ts
@@ -1,10 +1,18 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable('items', (table) => {
+		table.increments('id').primary().notNullable()
+		table.string('name').notNullable()
+		table.string('email').notNullable()
+		table.string('whatsapp').notNullable()
+		table.decimal('latitude').notNullable()
+		table.decimal('longitude').notNullable()
+		table.string('city').notNullable()
+		table.string('uf').notNullable()
+	})
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+	await knex.schema.dropTable('items')
 }
-
