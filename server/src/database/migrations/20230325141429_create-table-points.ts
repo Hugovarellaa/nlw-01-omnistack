06182ca9d52d6fb9b032a3034f95665b4ce18e40
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-10-08 12:45:34 -0300

    feat: update database

--- server/src/database/migrations/20230325141429_create-table-points.ts
+++ server/src/database/migrations/20230325141429_create-table-points.ts
@@ -3,6 +3,7 @@ import { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
 	await knex.schema.createTable('points', (table) => {
 		table.increments('id').primary().notNullable()
+		table.string('image').notNullable()
 		table.string('name').notNullable()
 		table.string('email').notNullable()
 		table.string('whatsapp').notNullable()
