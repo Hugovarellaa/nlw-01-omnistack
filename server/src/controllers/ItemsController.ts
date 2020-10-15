c47840b17bebe8b0b19029b753c031749706805e
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-10-15 13:12:10 -0300

    refactor: routes get/items - using controller

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -1,1 +1,18 @@
-export class ItemsController {}
+import { Request, Response } from 'express'
+import { knex } from '../database'
+
+export class ItemsController {
+	async index(request: Request, response: Response) {
+		const items = await knex('items').select()
+
+		const serializedItems = items.map((item) => {
+			return {
+				id: item.id,
+				name: item.name,
+				image_url: `http://localhost:3333/uploads/${item.image}`,
+			}
+		})
+
+		return response.json(serializedItems)
+	}
+}
