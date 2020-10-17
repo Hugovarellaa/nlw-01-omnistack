d1abde897ad25b80ee8ff664773d65d342f64f0f
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-10-17 15:55:25 -0300

    feat: install and config CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
+import cors from 'cors'
 import express from 'express'
 import { resolve } from 'node:path'
 import { appRoutes } from './routes'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(appRoutes)
 
