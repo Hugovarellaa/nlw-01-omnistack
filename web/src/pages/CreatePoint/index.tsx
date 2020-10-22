6b60058d8eeb21d96c710199c3e74284dea87312
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-10-22 18:14:23 -0300

    feat: Layout CreatePoint page

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,9 +1,48 @@
+import { FiArrowLeft } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import logoImg from '../../assets/logo.svg'
 import './CreatePoint.css'
 
 export function CreatePoint() {
   return (
-    <div>
-      <h1>Create Point</h1>
+    <div id="page-create-point">
+      <header>
+        <img src={logoImg} alt="" />
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para a Home
+        </Link>
+      </header>
+
+      <form>
+        <h1>
+          Cadastro do
+          <br /> ponto de coleta
+        </h1>
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input type="text" name="name" id="name" />
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+          </legend>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Ítens de coleta</h2>
+          </legend>
+        </fieldset>
+      </form>
     </div>
   )
 }
