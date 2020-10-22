b038ed944ee4843bf980c75e93da7296a407aace
Hugo Alves Varella <hugovarellaa@gmail.com>
2020-10-22 18:25:09 -0300

    feat: Layout CreatePoint page

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -29,17 +29,47 @@ export function CreatePoint() {
             <label htmlFor="name">Nome da entidade</label>
             <input type="text" name="name" id="name" />
           </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" id="email" />
+            </div>
+
+            <div className="field">
+              <label htmlFor="whatspp">WhatsApp</label>
+              <input type="text" name="whatspp" id="whatspp" />
+            </div>
+          </div>
         </fieldset>
 
         <fieldset>
           <legend>
             <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
           </legend>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado (UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+              </select>
+            </div>
+
+            <div className="field">
+              <label htmlFor="city">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma Cidade</option>
+              </select>
+            </div>
+          </div>
         </fieldset>
 
         <fieldset>
           <legend>
             <h2>Ítens de coleta</h2>
+            <span>Selecione um ou mais ítens abaixo</span>
           </legend>
         </fieldset>
       </form>
