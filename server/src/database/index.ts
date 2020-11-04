eaef6d2cfcab56fbf744d18bcd8ee8f4d77b58ca
hugo <hugovarellaa@gmail.com>
2020-11-04 11:21:50 -0300

    feat: Refact config Database

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,14 +1,15 @@
 import { Knex, knex as setupKnex } from 'knex'
+import { resolve } from 'node:path'
 
 export const knexConfig: Knex.Config = {
 	client: 'sqlite',
 	connection: {
-		filename: './src/database/app.db',
+		filename: resolve(__dirname, 'app.db'),
 	},
 	useNullAsDefault: true,
 	migrations: {
 		extension: 'ts',
-		directory: './src/database/migrations',
+		directory: resolve(__dirname, 'migrations'),
 	},
 }
 
