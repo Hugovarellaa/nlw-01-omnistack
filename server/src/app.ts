9d73f3905e681c281fdb6647b57022cfa0d4ac42
hugo <hugovarellaa@gmail.com>
2020-11-07 17:49:46 -0300

    feat: refactoring pointsRoutes

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,8 +6,8 @@ import { itemsRoutes } from './routes/items.routes'
 export const app = express()
 
 app.use(express.json())
+
 app.use(itemsRoutes)
+app.use(pointsRoutes)
 
 app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')))
-
-app.use(pointsRoutes)
