130989769c2819ae2d606eb5809b2569998829af
hugo <hugovarellaa@gmail.com>
2020-11-08 17:50:09 -0300

    feat: create controller itemsController

---
+++ server/src/controllers/ItemsController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import { knex } from '../database'
+
+export class ItensController {
+	async show(request: Request, response: Response) {
+		const items = await knex('items').select('*')
+
+		const serializedItems = items.map((item) => {
+			return {
+				id: item.id,
+				name: item.name,
+				image_url: `http://localhost:3333/uploads/${item.image}`,
+			}
+		})
+
+		return response.json(serializedItems)
+	}
+}
