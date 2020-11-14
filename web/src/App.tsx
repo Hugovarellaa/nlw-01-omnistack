648b5f72ef396bab74cb1b987046d23b274011ee
hugo <hugovarellaa@gmail.com>
2020-11-14 20:05:27 -0300

    feat: started project

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,11 @@
+import { Header } from './components/Header'
+import './styles/App.css'
+
 export function App() {
   return (
     <>
       <div>
+        <Header />
         <h1>Ola mundo</h1>
       </div>
     </>
