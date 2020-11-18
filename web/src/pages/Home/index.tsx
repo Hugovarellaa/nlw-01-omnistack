d7ac28654df3fd400f9ca1998c24bc850806665c
hugo <hugovarellaa@gmail.com>
2020-11-18 20:22:48 -0300

    feat: Styles Layout Home

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,10 +1,29 @@
+import { FiLogIn } from 'react-icons/fi'
+import { Header } from '../../components/Header'
 import './Home.css'
 
 export function Home() {
   return (
     <>
-      <div>
-        <h1>Home</h1>
+      <div id="page-home">
+        <div className="content">
+          <Header />
+
+          <main>
+            <h1>Seu marketplace de coleta de resíduos.</h1>
+            <p>
+              Ajudamos pessoas a encontrarem pontos de coleta de forma
+              eficiente.
+            </p>
+
+            <a href="#">
+              <span>
+                <FiLogIn />
+              </span>
+              <strong>Cadastre um ponto de coleta</strong>
+            </a>
+          </main>
+        </div>
       </div>
     </>
   )
