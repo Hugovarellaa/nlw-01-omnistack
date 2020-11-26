e61dd140da756f4ae72860c4e17a418656a69e20
hugo <hugovarellaa@gmail.com>
2020-11-26 12:24:33 -0300

    feat: logic mark current item

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -27,18 +27,30 @@ export function CreatePoint() {
   const [ufs, setUfs] = useState<string[]>([])
   const [cities, setCities] = useState<string[]>([])
 
+  const [position, setPosition] = useState<[number, number]>([0, 0])
+
   const [selectedUf, setSelectedUf] = useState('0')
   const [selectedCities, setSelectedCities] = useState('0')
 
-  const [position, setPosition] = useState<[number, number]>([
-    -15.4568511, -47.6065576,
+  const [selectedItems, setSelectedItems] = useState<number[]>([])
+
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([
+    0, 0,
   ])
 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    whatsapp: '',
+  })
+
+  // Items API
   async function loadItemsInApi() {
     const response = await api.get('/items')
     setItems(response.data)
   }
 
+  // IBGE API
   async function getApiIBGEUfs() {
     const response = await axios.get<IBGEUfResponse[]>(
       `https://servicodados.ibge.gov.br/api/v1/localidades/estados
@@ -48,6 +60,7 @@ export function CreatePoint() {
     setUfs(ufInitials)
   }
 
+  // IBGE API
   async function getApiIBGECities() {
     const response = await axios.get<IBGECitiesResponse[]>(
       `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/distritos`,
@@ -58,29 +71,66 @@ export function CreatePoint() {
     setCities(citiesName)
   }
 
+  // IBGE API
   function handleSelectedUf(event: ChangeEvent<HTMLSelectElement>) {
     const uf = event.target.value
     setSelectedUf(uf)
   }
 
+  // IBGE API
   function handleSelectedCity(event: ChangeEvent<HTMLSelectElement>) {
     const city = event.target.value
     setSelectedCities(city)
   }
 
+  // Mapa
   function handleMapClick(event: LeafletMouseEvent) {
     setPosition([event.latlng.lat, event.latlng.lng])
   }
 
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    event.preventDefault()
+    const { name, value } = event.target
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    })
+  }
+
+  function handleSelectedItem(id: number) {
+    // Marca o item como Ativo, Caso ele ja esteja ativo ele desmarcar
+
+    const alreadySelected = selectedItems.findIndex((item) => item === id)
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter((item) => item !== id)
+      setSelectedItems(filteredItems)
+    } else {
+      setSelectedItems((oldItem) => [...oldItem, id])
+    }
+  }
+
+  // mapa
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords
+
+      setInitialPosition([latitude, longitude])
+    })
+  }, [])
+
+  // IBGE API
   useEffect(() => {
     getApiIBGEUfs()
   }, [])
 
+  // IBGE API
   useEffect(() => {
     getApiIBGECities()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedUf])
 
+  // Items API
   useEffect(() => {
     loadItemsInApi()
   }, [])
@@ -108,17 +158,32 @@ export function CreatePoint() {
 
           <div className="field">
             <label htmlFor="name">Nome da entidade</label>
-            <input type="text" id="name" name="name" />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={handleInputChange}
+            />
           </div>
 
           <div className="field-group">
             <div className="field">
               <label htmlFor="email">E-mail</label>
-              <input type="email" id="email" name="email" />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                onChange={handleInputChange}
+              />
             </div>
             <div className="field">
               <label htmlFor="whatsapp">Whatsapp</label>
-              <input type="text" id="whatsapp" name="whatsapp" />
+              <input
+                type="text"
+                id="whatsapp"
+                name="whatsapp"
+                onChange={handleInputChange}
+              />
             </div>
           </div>
         </fieldset>
@@ -131,8 +196,12 @@ export function CreatePoint() {
 
           {/* Maps */}
           {/* @ts-ignore erro na própria API - maneira sugerida na comunidade */}
-          <Map center={position} onclick={handleMapClick}>
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
             <Marker position={position} />
           </Map>
 
@@ -183,7 +252,11 @@ export function CreatePoint() {
 
           <ul className="items-grid">
             {items.map((item) => (
-              <li key={item.id}>
+              <li
+                key={item.id}
+                onClick={() => handleSelectedItem(item.id)}
+                className={selectedItems.includes(item.id) ? 'selected' : ''}
+              >
                 <img src={item.image_url} alt={item.name} />
                 <span>{item.name}</span>
               </li>
