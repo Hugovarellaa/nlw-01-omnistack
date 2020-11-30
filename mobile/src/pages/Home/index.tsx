0ba8e9084a1afd0bb35e7c3249e6aaa3db7f2f35
hugo <hugovarellaa@gmail.com>
2020-11-30 14:56:26 -0300

    feat: create Home Page

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import { styles } from './styles'
+
+export function Home() {
+  return (
+    <View style={styles.main}>
+      <Text>Home</Text>
+    </View>
+  )
+}
