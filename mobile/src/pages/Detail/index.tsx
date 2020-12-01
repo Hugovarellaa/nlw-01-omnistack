4bc3dcf909eaee5c6fd5b3e56b714c21b6fabb8e
hugo <hugovarellaa@gmail.com>
2020-12-01 14:58:42 -0300

    feat: create Detail page

---
+++ mobile/src/pages/Detail/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import { styles } from './styles'
+
+export function Detail() {
+  return (
+    <View style={styles.container}>
+      <Text></Text>
+    </View>
+  )
+}
