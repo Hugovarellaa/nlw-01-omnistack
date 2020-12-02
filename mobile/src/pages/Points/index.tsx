5fc2570c88811051aac639bc3d2ce9bdfbf6b58c
hugo <hugovarellaa@gmail.com>
2020-12-02 15:01:19 -0300

    feat: create Point page

---
+++ mobile/src/pages/Points/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import { styles } from './styles'
+
+export function Points() {
+  return (
+    <View style={styles.container}>
+      <Text>Points</Text>
+    </View>
+  )
+}
