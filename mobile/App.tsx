17b8381085680e2b9662623644f20e90d7f45129
hugo <hugovarellaa@gmail.com>
2020-12-09 17:55:14 -0300

    feat: install fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,22 @@
-import { StatusBar } from 'expo-status-bar'
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
+import { StatusBar } from 'react-native'
 import { Routes } from './src/routes'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Ubuntu_700Bold,
+    Roboto_400Regular,
+    Roboto_500Medium,
+  })
+
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="dark-content"
+      />
       <Routes />
     </>
   )
--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,14 +1,16 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Detail } from '../screens/Detail'
 import { Home } from '../screens/Home'
+import { Point } from '../screens/Points'
 
 const { Navigator, Screen } = createNativeStackNavigator()
 
 export function AppRoutes() {
   return (
     <Navigator>
-      <Screen name="detail" component={Detail} />
       <Screen name="home" component={Home} />
+      <Screen name="detail" component={Detail} />
+      <Screen name="point" component={Point} />
     </Navigator>
   )
 }
